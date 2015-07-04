4cc701fcb7e7964eb51e536307c9286d0d1812f8
tsukumaru <came.and.rabbit@gmail.com>
2015-07-04 11:47:06 +0900

    spec3: implement

--- src/Recipe_Manage.java
+++ src/Recipe_Manage.java
@@ -1,8 +1,22 @@
-/**
- * Created by MaruyamaJun on 2015/07/04.
- */
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Recipe_Manage {
     public static void main(String[] args){
-        System.out.println("オムライス");
+
+        //ファイル入出力処理における例外処理
+        try {
+            //ファイル読み込み
+            BufferedReader br = new BufferedReader(new FileReader(new File("recipe-title-data.txt")));
+
+            String title = br.readLine();
+            System.out.println(title);
+
+            br.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
