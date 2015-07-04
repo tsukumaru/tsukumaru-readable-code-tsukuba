235c600531d939a9ff5d65fd8a7381587ea5477d
tsukumaru <came.and.rabbit@gmail.com>
2015-07-04 11:31:06 +0900

    spec1: implement

---
+++ src/Recipe_Manage.java
@@ -0,0 +1,8 @@
+/**
+ * Created by MaruyamaJun on 2015/07/04.
+ */
+public class Recipe_Manage {
+    public static void main(String[] args){
+        System.out.println("オムライス");
+    }
+}
--- src/sample.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by MaruyamaJun on 2015/07/04.
- */
-public class sample {
-    public static void main(){}
-}
